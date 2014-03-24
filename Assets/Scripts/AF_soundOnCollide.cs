f5445fbaf3a018ba0055c8b6c14e1f9d460957c5
addisn <addison.freeman@trinity.edu>
2014-03-24 17:29:24 -0500

    added Audio scripts and files
    many of the folders are for an external audio editor that I want to play
    around with. I may have changed some of the parameters on the prefabs in
    trying to get audio play on collision.

---
+++ Assets/Scripts/AF_soundOnCollide.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AF_soundOnCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		audio.PlayOneShot(other.gameObject.audio.clip);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
