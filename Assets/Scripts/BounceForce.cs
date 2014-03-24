8630a3548cd6d924431622da4a201a2831806c8f
Matthew Hibbs <mhibbs@mhibbsair.local>
2014-03-24 13:18:03 -0500

    Another commit

---
+++ Assets/Scripts/BounceForce.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position.y < 0.0) {
+			print(rigidbody.velocity);
+			rigidbody.AddForce (Vector3.Scale(rigidbody.velocity, new Vector3(0,-2,0)));
+		}
+	}
+}
